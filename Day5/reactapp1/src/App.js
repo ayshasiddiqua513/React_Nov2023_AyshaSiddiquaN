8b0010e49566d4521cc63fa034a045510aaa1505
ayshasiddiqua513 <117656371+ayshasiddiqua513@users.noreply.github.com>
2023-11-16 12:56:38 +0530

    DAY5

---
+++ Day5/reactapp1/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LifeCycle from './components/LifeCycle';
+function App() {
+  return (
+    <div className="App">
+      <LifeCycle/>
+    </div>
+  );
+}
+
+export default App;
